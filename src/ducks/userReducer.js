8f26576178a62ace1b82c6bcf05103869106bd6d
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-01-27 13:54:37 -0700

    remove console log

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -9,12 +9,9 @@ const initialState = {
 const REQUEST_USER_DATA = 'REQUEST_USER_DATA'
 
 export function requestUserData(){
-  console.log('hit')
   const data = axios.get('/auth/user-data').then(results => {
-    console.log(results)
     return results.data
   }).catch(err => console.log(err))
-  console.log(data)
   return {
     type: REQUEST_USER_DATA,
     payload: data
